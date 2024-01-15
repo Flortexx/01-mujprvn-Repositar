2340b5cf05cec27679af786b4bea4a619cb09d7f
Flortexx <145441012+Flortexx@users.noreply.github.com>
2024-01-15 15:01:31 +0100

    Add files via upload

---
+++ 21-superhry/index3.js
@@ -0,0 +1,7 @@
+function travel(){
+    window.location.href = 'http://127.0.0.1:5500/01-mujprvn-Repositar-main/01-mujprvn-Repositar-main/IT1/21-superhry/pexeso1.html';
+}
+
+function travel(){
+    window.location.href = 'http://127.0.0.1:5500/01-mujprvn-Repositar-main/01-mujprvn-Repositar-main/IT1/21-superhry/pi%C5%A1kvorky1.html'
+}
